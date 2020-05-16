8556b77083599992a4ba8842beec60331f66caad
chiaraco <cordierchiara@gmail.com>
2020-05-17 00:22:37 +0200

    16/05/2020

--- projet_BDR/monappli/views.py
+++ projet_BDR/monappli/views.py
@@ -23,12 +23,30 @@ def donnees(request):
 	return render(request,'choix_table.tmpl')
 	
 def pays(request):
-	return render(request, 'pays.tmpl', 
-		{                                          
-            'pays': Pays.objects.all().order_by('nom_pays'),
-            'nb': Pays.objects.count()-1,
-		    'rien': Pays.objects.count()==0
-		})
+	if not 'pays_iso' in request.GET.keys():
+		return render(request, 'pays.tmpl', 
+			{                                          
+        	    'pays': Pays.objects.all().order_by('nom_pays'),
+        	    'nb': Pays.objects.count()-1,
+			    'rien': Pays.objects.count()==0
+			})
+		 
+	else :
+		pays_iso=request.GET['pays_iso']
+		pa_iso=Pays.objects.none()
+		if 'chercher_iso' in request.GET.keys():
+			iso=Pays.objects.filter(iso__icontains=pays_iso)
+			pa_iso=iso
+		if 'chercher_pays' in request.GET.keys():
+			pa=Pays.objects.filter(nom_pays__icontains=pays_iso)
+			pa_iso=pa_iso | pa
+			
+		return render(request, 'pays.tmpl', 
+			{                                          
+        	   	'pays': pa_iso.order_by('nom_pays'),
+        	   	'nb': pa_iso.count(),
+	    	    'rien': pa_iso.count()==0
+	    	})
 
 def ville(request):
 	if not 'pays_ville' in request.GET.keys(): 
@@ -38,26 +56,18 @@ def ville(request):
            	'nb': Ville.objects.count()-1,
 	        'rien': Ville.objects.count()==0
 	    })
+	    
 	else :
-		vi_b=False
-		pa_b=False
+		vi_pa=Ville.objects.none()
 		pays_ville=request.GET['pays_ville']
 		if 'chercher_ville' in request.GET.keys():
 			vi=Ville.objects.filter(nom_ville__icontains=pays_ville)
-			vi_b=True
-			print("vi : ", type(vi))
+			vi_pa=vi
 		if 'chercher_pays' in request.GET.keys():
 			pays=Pays.objects.filter(nom_pays__icontains=pays_ville)
 			pa=Ville.objects.filter(nom_pays__in=pays)
-			pa_b=True
-		if pa_b and vi_b: vi_pa=vi | pa
-		elif pa_b: vi_pa=pa
-		elif vi_b: vi_pa=vi
-		else: return render(request, 'ville.tmpl', 
-		{                                          
-	        'rien': True
-	    })
-		print("vi_pa" , type(vi_pa))
+			vi_pa=vi_pa | pa
+
 		return render(request, 'ville.tmpl', 
 		{                                          
            	'ville': vi_pa.order_by('nom_ville'),
@@ -82,12 +92,37 @@ def compagnie(request):
 	    })
 
 def aeroport(request):
-	return render(request, 'aeroport.tmpl', 
-	    {                                          
-	        'aeroport': Aeroport.objects.all().order_by('nom_aeroport'),
-	        'nb': Aeroport.objects.count()-1,
-	        'rien': Aeroport.objects.count()==0
-	    })
+	if not 'aero' in request.GET.keys():
+		return render(request, 'aeroport.tmpl', 
+		    {                                          
+		        'aeroport': Aeroport.objects.all().order_by('nom_aeroport'),
+		        'nb': Aeroport.objects.count()-1,
+		        'rien': Aeroport.objects.count()==0
+		    })
+		 
+	else :
+		liste=Aeroport.objects.none()
+		aero=request.GET['aero']
+		if 'chercher_aero' in request.GET.keys():
+			nom=Aeroport.objects.filter(nom_aeroport__icontains=aero)
+			liste=nom
+		if 'chercher_iata' in request.GET.keys():
+			iata=Aeroport.objects.filter(iata__icontains=aero)
+			liste=liste | iata
+		if 'chercher_oaci' in request.GET.keys():
+			oaci=Aeroport.objects.filter(oaci__icontains=aero)
+			liste=liste | oaci
+		if 'chercher_ville' in request.GET.keys():
+			ville=Ville.objects.filter(nom_ville__icontains=aero)
+			ae=Aeroport.objects.filter(ville__in=ville)
+			liste=liste | ae
+			
+		return render(request, 'aeroport.tmpl', 
+			{                                          
+       	    	'aeroport': vi_pa.order_by('nom_aeroport'),
+        	   	'nb': vi_pa.count(),
+	    	    'rien': vi_pa.count()==0
+	    	})
 	    
 def accident(request):
 	return render(request, 'accident.tmpl', 
